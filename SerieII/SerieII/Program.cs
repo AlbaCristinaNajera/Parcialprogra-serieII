fc08e42edd8ff83dd27ca15801c2088825c2c55b
Cristina_nm <anajeram@miumg.edu.gt>
2024-03-09 11:58:42 -0600

    commit 1 primer parcial

---
+++ SerieII/SerieII/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerieII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primer parcial de progrmación");
+            Console.Read();
+        }
+    }
+}
